c017ccbbb0dba2790baa2fa3f954106a09c5a834
GHERARDI-JOAO <gherardijoao@gmail.com>
2025-03-12 22:31:33 -0300

    Exercicios js

---
+++ javascriptBegginer/exerciciosCondicional/script.js
@@ -0,0 +1,15 @@
+// 📝 Exercícios de Condicionais em JavaScript
+
+// 01) Verifique se a sua idade é maior do que a de algum parente
+// Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
+
+var myAge = 20;
+var familiarAge = 40;
+
+if (myAge > familiarAge) {
+  console.log("É maioir");
+} else if (myAge < familiarAge) {
+  console.log("É menor");
+} else if (myAge === familiarAge) {
+  console.log("É igual");
+}
