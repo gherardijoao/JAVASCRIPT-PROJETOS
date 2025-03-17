47ca8fcccc83a77624ba432bddf165100faef168
GHERARDI-JOAO <gherardijoao@gmail.com>
2025-03-17 15:59:29 -0300

    exercicios iteração

---
+++ javascriptBeginner/arraysLoops/script.js
@@ -0,0 +1,9 @@
+var videoGames = ["Switch", "PS4", "XBOX"];
+
+videoGames.push("3DS");
+videoGames.pop();
+console.log(videoGames);
+
+for (var numero = 1; numero <= 100; numero++) {
+  console.log(numero);
+}
