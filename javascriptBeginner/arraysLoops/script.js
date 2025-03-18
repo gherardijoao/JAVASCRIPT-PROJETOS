df7ce0b0be17d61d2eab5b9dfc263735bc3170f9
GHERARDI-JOAO <gherardijoao@gmail.com>
2025-03-18 00:14:23 -0300

    loop e arrays exercícios

--- javascriptBeginner/arraysLoops/script.js
+++ javascriptBeginner/arraysLoops/script.js
@@ -1,9 +1,39 @@
-var videoGames = ["Switch", "PS4", "XBOX"];
+var videoGames = ["Switch", "PS4", "XBOX", "3DS"];
 
-videoGames.push("3DS");
-videoGames.pop();
-console.log(videoGames);
+for (var item = 0; item < 4; item++) {
+  console.log(videoGames[item]);
 
-for (var numero = 1; numero <= 100; numero++) {
-  console.log(numero);
+  if (videoGames[item] === "PS4") {
+    break;
+  }
 }
+
+// 01) Crie uma array com os anos que o Brasil ganhou a copa
+// 1959, 1962, 1970, 1994, 2002
+
+var brasilCampeao = [1959, 1962, 1970, 1994, 2002];
+
+// 02) Interaja com a array utilizando um loop, para mostrar
+// no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
+
+for (var i = 0; i < brasilCampeao.length; i++) {
+  console.log(`O brasil ganhou a copa de ${brasilCampeao[i]}`);
+}
+
+// 03) Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
+// var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância']
+
+var frutas = ["Banana", "Maçã", "Pera", "Uva", "Melância"];
+
+for (var i = 0; i < frutas.length; i++) {
+  console.log(frutas[i]);
+
+  if (frutas[i] === "Pera") {
+    break;
+  }
+}
+
+// 04) Coloque a última fruta da array acima em uma variável,
+// sem remover a mesma da array.
+
+var ultimaFruta = frutas[frutas.length - 1];
