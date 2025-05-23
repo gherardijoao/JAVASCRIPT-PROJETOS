6f9c36ec72379e43c680c6a408eb5aa29c88d8ea
GHERARDI-JOAO <gherardijoao@gmail.com>
2025-05-23 00:35:14 -0300

    exercicios math

--- javascriptBeginner/exerciciosNumberMath/script.js
+++ javascriptBeginner/exerciciosNumberMath/script.js
@@ -1,3 +1,11 @@
-const preco = 2.99;
+// Retorne um número aleatório
+// entre 1050 e 2000
+let numero = 0;
 
-console.log(preco.toFixed(1));
+numero = Math.floor(Math.random() * (2000 - 1050 + 1)) + 1050;
+
+console.log(numero);
+
+// Retorne o maior número da lista abaixo
+maior = Math.max(4, 5, 20, 8, 9);
+console.log(maior);
